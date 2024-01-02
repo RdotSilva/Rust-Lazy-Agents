754ddc15f543cb8d9e3560c32ab0c2c584d544af
RdotSilva <RyandotSilva@gmail.com>
2024-01-02 18:14:53 +0000

    Add get_user_response function

--- src/helpers/command_line.rs
+++ src/helpers/command_line.rs
@@ -3,3 +3,29 @@ use crossterm::{
     ExecutableCommand,
 };
 use std::io::{stdin, stdout};
+
+/// Prompt a user with a question that will expect a response
+/// This will be used when we ask the user what they want to build
+/// # Arguments
+///
+/// * `question` - The question to ask the user
+pub fn get_user_response(question: &str) -> String {
+    let mut stdout: std::io::Stdout = stdout();
+
+    // Print the question in a specific color
+    stdout.execute(SetForegroundColor(Color::Blue)).unwrap();
+    println!("");
+    println!("{}", question);
+
+    // Reset Color
+    stdout.execute(ResetColor).unwrap();
+
+    // Read user input
+    let mut user_response: String = String::new();
+    stdin()
+        .read_line(&mut user_response)
+        .expect("Failed to read response");
+
+    // Trim whitespace and return the user response
+    return user_response.trim().to_string();
+}
