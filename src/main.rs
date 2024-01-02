4a725a255b4a1bba14a02505668d566f1f1d5c61
RdotSilva <RyandotSilva@gmail.com>
2024-01-02 18:19:56 +0000

    Update main to use new user request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod ai_functions;
 mod helpers;
 mod models;
+
+use helpers::command_line::get_user_response;
 fn main() {
-    println!("Hello, world!");
+    let user_request: String = get_user_response("What web server would you like to build today?");
+
+    dbg!(user_request)
 }
