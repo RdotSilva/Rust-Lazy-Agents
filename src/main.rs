68d3d77709ba2374c3a708811cd3a550a2cb5829
RdotSilva <RyandotSilva@gmail.com>
2024-01-03 20:16:41 +0000

    Fix build error

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ use helpers::command_line::get_user_response;
 fn main() {
     let user_request: String = get_user_response("What web server would you like to build today?");
 
-    dbg!(user_request)
+    dbg!(user_request);
 }
