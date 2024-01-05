c2aa6ac5b19d96cff7ed17c5190906e1376ed9d6
RdotSilva <RyandotSilva@gmail.com>
2024-01-05 20:34:14 +0000

    Add basic logic for api calls

--- src/apis/call_request.rs
+++ src/apis/call_request.rs
@@ -1,1 +1,18 @@
+use crate::models::general::llm::Message;
+use dotenv::dotenv;
+use reqwest::Client;
+use std::env;
 
+// Call Large Language Model (i.e. GPT-4)
+pub async fn call_gpt(message: Vec<Message>) {
+    dotenv().ok();
+
+    // Extract API keys
+    let api_key: String =
+        env::var("OPEN_AI_KEY").expect("OPEN_AI_KEY not found in environment variables");
+    let org_key: String =
+        env::var("OPEN_AI_ORG").expect("OPEN_AI_ORG not found in environment variables");
+
+    // Confirm endpoint
+    let url: &str = "https://api.openai.com/v1/chat/completions";
+}
