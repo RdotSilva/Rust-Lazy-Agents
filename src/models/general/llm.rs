f98f897facc3987f1267af1327e7104c60a234f7
RdotSilva <RyandotSilva@gmail.com>
2024-01-07 18:22:08 +0000

    Add ChatCompletion struct

--- src/models/general/llm.rs
+++ src/models/general/llm.rs
@@ -5,3 +5,14 @@ pub struct Message {
     pub role: String,
     pub content: String,
 }
+
+// Represents the data and options passed into ChatGPT
+#[derive(Debug, Serialize, Clone)]
+pub struct ChatCompletion {
+    // The ChatGPT model to use
+    pub model: String,
+    // The messages to pass into the model
+    pub messages: Vec<Message>,
+    // The temperature setting used to control the ChatGPT response
+    pub temperature: f32,
+}
