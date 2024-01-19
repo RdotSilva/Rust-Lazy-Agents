cebeec24a8fa0b7ab7684286b2cda09cefbc58d3
RdotSilva <RyandotSilva@gmail.com>
2024-01-19 19:53:04 +0000

    Refactor extend_ai_function to return Message

--- src/helpers/general.rs
+++ src/helpers/general.rs
@@ -1,3 +1,5 @@
+use crate::models::general::llm::Message;
+
 /// Extend AI function to encourage specific output
 /// This will help us get a specific output that we are expecting
 /// This will run the AI function, get the string out of the function and extend the function string
@@ -5,7 +7,7 @@
 ///
 /// * `ai_func` - The the function we want to extend
 //
-pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) {
+pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -> Message {
     let ai_function_str: &str = ai_func(func_input);
 
     // Extend the string to encourage only printing the output
@@ -17,7 +19,11 @@ pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) {
         ai_function_str, func_input
     );
 
-    dbg!(msg);
+    // Return message
+    Message {
+        role: "system".to_string(),
+        content: msg,
+    }
 }
 
 #[cfg(test)]
@@ -27,6 +33,9 @@ mod tests {
 
     #[test]
     fn tests_extending_ai_function() {
-        extend_ai_function(convert_user_input_to_goal, "Dummy Variable")
+        let extended_msg: Message =
+            extend_ai_function(convert_user_input_to_goal, "dummy variable");
+        dbg!(&extended_msg);
+        assert_eq!(extended_msg.role, "system".to_string());
     }
 }
