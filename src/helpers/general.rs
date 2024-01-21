f0330544fe246c56b7245d65d236b9a0e09f5a15
RdotSilva <RyandotSilva@gmail.com>
2024-01-21 20:35:52 +0000

    Add ai_task_request_decoded function

--- src/helpers/general.rs
+++ src/helpers/general.rs
@@ -1,3 +1,5 @@
+use serde::de::DeserializeOwned;
+
 use crate::apis::call_request::call_gpt;
 use crate::helpers::command_line::PrintCommand;
 use crate::models::general::llm::Message;
@@ -59,6 +61,56 @@ pub async fn ai_task_request(
             .await
             .expect("Failed twice to call OpenAI"),
     }
+}
+
+/// Extend AI function to encourage specific output
+/// This will help us get a specific output that we are expecting
+/// This will run the AI function, get the string out of the function and extend the function string
+/// # Arguments
+///
+/// * `ai_func` - The the function we want to extend
+//
+pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -> Message {
+    let ai_function_str: &str = ai_func(func_input);
+
+    // Extend the string to encourage only printing the output
+    let msg: String = format!(
+        "FUNCTION: {}
+  INSTRUCTION: You are a function printer. You ONLY print the results of functions.
+  Nothing else. No commentary. Here is the input to the function: {}.
+  Print out what the function will return.",
+        ai_function_str, func_input
+    );
+
+    // Return message
+    Message {
+        role: "system".to_string(),
+        content: msg,
+    }
+}
+
+/// Performs call to LLM GPT - Decoded version
+/// We will get the string back from LLM and decode the string and create a struct
+/// # Arguments
+///
+/// * `msg_context` - The message we are going to send as a function input
+/// * `agent_position` - The type of agent making the request
+/// * `agent_operation` - The operation the agent is actually doing
+/// * `function_pass` - The function that we are passing
+///
+pub async fn ai_task_request_decoded<T: DeserializeOwned>(
+    msg_context: String,
+    agent_position: &str,
+    agent_operation: &str,
+    function_pass: for<'a> fn(&'a str) -> &'static str,
+) -> T {
+    let llm_response: String =
+        ai_task_request(msg_context, agent_position, agent_operation, function_pass).await;
+
+    let decoded_response: T = serde_json::from_str(llm_response.as_str())
+        .expect("Failed to decode ai response from serde_json");
+
+    return decoded_response;
 }
 
 #[cfg(test)]
