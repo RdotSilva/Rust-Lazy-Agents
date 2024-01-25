8dba97335362ea97dee47c668a3cc2a0b4ca40bc
RdotSilva <RyandotSilva@gmail.com>
2024-01-25 18:24:57 +0000

    Add BasicAgent struct

--- src/models/agent_basic/basic_agent.rs
+++ src/models/agent_basic/basic_agent.rs
@@ -1,3 +1,5 @@
+use crate::models::general::llm::Message;
+
 /// Enum to describe the different states of an agent
 #[derive(Debug, PartialEq)]
 pub enum AgentState {
@@ -9,4 +11,18 @@ pub enum AgentState {
     UnitTesting,
     /// Finished state
     Finished,
+}
+
+/// Represents a basic agent
+#[derive(Debug)]
+pub struct BasicAgent {
+    /// The main objective of the agent
+    pub objective: String,
+    /// The position of the agent
+    pub position: String,
+    /// The current state of an agent
+    pub state: AgentState,
+    /// The memory of an agent
+    /// This is where we will store any conversation history
+    pub memory: Vec<Message>,
 }
