d0ae5e3e7088706d1c25ac600a5df55e9730367d
RdotSilva <RyandotSilva@gmail.com>
2024-01-26 20:59:40 +0000

    Add TODO for FactSheet

---
+++ src/models/agents/agent_traits.rs
@@ -0,0 +1,7 @@
+use crate::models::agent_basic::basic_agent::BasicAgent;
+use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
+use std::fmt::Debug;
+
+// TODO: Add FactSheet struct
+pub struct FactSheet {}
