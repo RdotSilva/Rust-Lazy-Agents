cc3900df34d3460521ae1426621d276499bac278
RdotSilva <RyandotSilva@gmail.com>
2024-01-27 19:21:24 +0000

    Add FactSheet struct

--- src/models/agents/agent_traits.rs
+++ src/models/agents/agent_traits.rs
@@ -3,5 +3,17 @@ use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
 
-// TODO: Add FactSheet struct
-pub struct FactSheet {}
+/// Represents a set of rules that the agents should follow
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+pub struct FactSheet {
+    /// Description of the project
+    pub project_description: String,
+    /// Scope of the project
+    pub project_scope: Option<ProjectScope>,
+    /// External URLS related to the project
+    pub external_urls: Option<Vec<String>>,
+    /// Backend code related to the project
+    pub backend_code: Option<String>,
+    /// API endpoint schema for the project
+    pub api_endpoint_schema: Option<Vec<RouteObject>>,
+}
