dc58fb4fd33dbefaf6edb0b4e6b262ffa3321293
RdotSilva <RyandotSilva@gmail.com>
2024-01-27 19:22:35 +0000

    Add ProjectScope struct

--- src/models/agents/agent_traits.rs
+++ src/models/agents/agent_traits.rs
@@ -3,6 +3,17 @@ use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
 
+/// Represents the scope of the project
+#[derive(Debug, Serialize, Deserialize, Clone, Copy, PartialEq)]
+pub struct ProjectScope {
+    /// Flag to determine if CRUD will be used
+    pub is_crud_required: bool,
+    /// Flag to determine if login/logout will be used
+    pub is_user_login_and_logout: bool,
+    /// Flag to determine if external URLS will be used
+    pub is_external_urls_required: bool,
+}
+
 /// Represents a set of rules that the agents should follow
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 pub struct FactSheet {
