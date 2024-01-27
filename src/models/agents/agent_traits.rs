056edea371a440ac164ba2a35ebb6a1bf0e386b6
RdotSilva <RyandotSilva@gmail.com>
2024-01-27 19:26:06 +0000

    Add RouteObject struct

--- src/models/agents/agent_traits.rs
+++ src/models/agents/agent_traits.rs
@@ -3,7 +3,24 @@ use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
 
+/// Represents the schema for the API endpoints
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+pub struct RouteObject {
+    /// Flag to determine if route is dynamic
+    pub is_route_dynamic: String,
+    /// The method of the route
+    pub method: String,
+    /// The request body of the route
+    pub request_body: serde_json::Value,
+    /// The response of the route
+    pub response: serde_json::Value,
+    /// The route path
+    pub route: String,
+}
+
 /// Represents the scope of the project
+/// This will be unknown when we first create a FactSheet
+/// This will be generated only after the agent does the initial work
 #[derive(Debug, Serialize, Deserialize, Clone, Copy, PartialEq)]
 pub struct ProjectScope {
     /// Flag to determine if CRUD will be used
@@ -19,7 +36,7 @@ pub struct ProjectScope {
 pub struct FactSheet {
     /// Description of the project
     pub project_description: String,
-    /// Scope of the project
+    /// Scope of the project (this will start as None when we start a project)
     pub project_scope: Option<ProjectScope>,
     /// External URLS related to the project
     pub external_urls: Option<Vec<String>>,
