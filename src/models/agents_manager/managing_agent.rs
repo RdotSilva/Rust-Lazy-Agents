b1f4bc6392f8a00961497bf7753571493b7dd444
RdotSilva <RyandotSilva@gmail.com>
2024-01-28 14:16:03 +0000

    Add ManagingAgent struct

--- src/models/agents_manager/managing_agent.rs
+++ src/models/agents_manager/managing_agent.rs
@@ -1,1 +1,12 @@
+use crate::models::agent_basic::basic_agent::{AgentState, BasicAgent};
+use crate::models::agents::agent_traits::FactSheet;
 
+/// Represents a managing agent
+#[derive(Debug)]
+pub struct ManagingAgent {
+    /// Attributes belonging to the agent
+    _attributes: BasicAgent,
+    /// Fact sheet for the agent
+    factsheet: FactSheet,
+    agents: Vec, // TODO: Add type
+}
