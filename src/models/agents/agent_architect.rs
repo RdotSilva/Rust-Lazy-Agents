dc56df3234893b4afcc49e147d024fbe88f87d7c
RdotSilva <RyandotSilva@gmail.com>
2024-01-31 20:54:00 +0000

    Add call_project_scope function

--- src/models/agents/agent_architect.rs
+++ src/models/agents/agent_architect.rs
@@ -29,4 +29,21 @@ impl AgentSolutionArchitect {
 
         Self { attributes }
     }
+
+    /// Retrieve Project Scope
+    async fn call_project_scope(&mut self, factsheet: &mut FactSheet) -> ProjectScope {
+        let msg_context: String = format!("{}", factsheet.project_description);
+
+        let ai_response: ProjectScope = ai_task_request_decoded::<ProjectScope>(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_project_scope),
+            print_project_scope,
+        )
+        .await;
+
+        factsheet.project_scope = Some(ai_response.clone());
+        self.attributes.update_state(AgentState::Finished);
+        return ai_response;
+    }
 }
