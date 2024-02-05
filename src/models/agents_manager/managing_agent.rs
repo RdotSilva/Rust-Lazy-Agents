f067526b91f46a078815879766c41a369cdae4a2
RdotSilva <RyandotSilva@gmail.com>
2024-02-05 16:23:38 +0000

    Add function to create new ManagingAgent

--- src/models/agents_manager/managing_agent.rs
+++ src/models/agents_manager/managing_agent.rs
@@ -18,4 +18,40 @@ pub struct ManagingAgent {
 }
 
 // TODO: Add ManagingAgent implementation
-impl ManagingAgent {}
+impl ManagingAgent {
+    pub async fn new(usr_req: String) -> Result<Self, Box<dyn std::error::Error>> {
+        let position: String = "Project Manager".to_string();
+
+        let attributes: BasicAgent = BasicAgent {
+            objective: "Manage agents who are building an excellent website for the user"
+                .to_string(),
+            position: position.clone(),
+            state: AgentState::Discovery,
+            memory: vec![],
+        };
+
+        let project_description: String = ai_task_request(
+            usr_req,
+            &position,
+            get_function_string!(convert_user_input_to_goal),
+            convert_user_input_to_goal,
+        )
+        .await;
+
+        let agents: Vec<Box<dyn SpecialFunctions>> = vec![];
+
+        let factsheet: FactSheet = FactSheet {
+            project_description,
+            project_scope: None,
+            external_urls: None,
+            backend_code: None,
+            api_endpoint_schema: None,
+        };
+
+        Ok(Self {
+            _attributes: attributes,
+            factsheet,
+            agents,
+        })
+    }
+}
