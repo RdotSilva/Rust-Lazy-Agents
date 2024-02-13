833026fee1ab7ce6145d731b4622a104da99249d
RdotSilva <RyandotSilva@gmail.com>
2024-02-13 17:59:23 +0000

    Add Discovery state logic

--- src/models/agents/agent_backend.rs
+++ src/models/agents/agent_backend.rs
@@ -186,7 +186,11 @@ impl SpecialFunctions for AgentBackendDeveloper {
     ) -> Result<(), Box<dyn std::error::Error>> {
         while self.attributes.state != AgentState::Finished {
             match &self.attributes.state {
-                AgentState::Discovery => {}
+                AgentState::Discovery => {
+                    self.call_initial_backend_code(factsheet).await;
+                    self.attributes.state = AgentState::Working;
+                    continue;
+                }
                 AgentState::Working => {}
                 AgentState::UnitTesting => {}
                 _ => {}
